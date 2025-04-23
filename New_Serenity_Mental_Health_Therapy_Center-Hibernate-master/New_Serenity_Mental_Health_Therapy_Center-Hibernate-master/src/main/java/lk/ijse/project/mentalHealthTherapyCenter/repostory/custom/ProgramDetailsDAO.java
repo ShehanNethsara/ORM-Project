66785fbcf5456ccfea8d4a7e5a77f3ae03f18dec
Shehan Nethsara <Shehan.nethsara2003@gmail.com>
2025-04-23 21:55:21 +0530

    Added orm project

---
+++ New_Serenity_Mental_Health_Therapy_Center-Hibernate-master/New_Serenity_Mental_Health_Therapy_Center-Hibernate-master/src/main/java/lk/ijse/project/mentalHealthTherapyCenter/repostory/custom/ProgramDetailsDAO.java
@@ -0,0 +1,7 @@
+package lk.ijse.project.mentalHealthTherapyCenter.repostory.custom;
+
+import lk.ijse.project.mentalHealthTherapyCenter.entity.ProgramDetails;
+import lk.ijse.project.mentalHealthTherapyCenter.repostory.CrudDAO;
+
+public interface ProgramDetailsDAO extends CrudDAO<ProgramDetails,String> {
+}
